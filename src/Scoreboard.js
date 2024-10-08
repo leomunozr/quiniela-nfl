73b0adae5beaf6d51fbdc3cc57bfe0d52ff64bdd
David Reyes <david@Davids-MacBook-Air.local>
2024-10-08 13:05:42 -0600

    Add opacity filter

--- src/Scoreboard.js
+++ src/Scoreboard.js
@@ -10,6 +10,7 @@ const ScoreboardContainer = styled(Paper)`
   box-shadow: ${({ playing }) => (playing ? "0 0 3px #8BD6A4" : "none")};
   display: flex;
   align-items: center;
+  filter: opacity(50%);
 `;
 
 const Score = styled(Grid2)`
