5b5bc637e83de0b576d3f387bd5f88716412d2d0
leomunozr <leo@leomunoz.dev>
2025-09-04 17:03:12 -0600

    New Season 🎉

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ import teamsData from "./data/teams";
 import fireworks from "./fireworks";
 import Accumulated from "./components/Accumulated";
 
-const IS_PLAYOFFS = true
+const IS_PLAYOFFS = false;
 
 const ContainerStyled = styled(Container)`
   padding-left: 0;
--- src/data/acc.js
+++ src/data/acc.js
@@ -1,9 +1,1 @@
-export const points = {
-  'Leo 🦁': 11.5,
-  'Gabriel': 11,
-  'David': 11,
-  'DIANA': 10.5,
-  'David 🌙': 10,
-  'Itzae': 5,
-  'Alexis': 3,
-};
+export const points = {};
