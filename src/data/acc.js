6f09a3e0c91c1761d149ac031409b00427da6dc6
leomunozr <leo@leomunoz.dev>
2026-01-21 11:37:16 -0600

    update acumulado

--- src/data/acc.js
+++ src/data/acc.js
@@ -1,11 +1,11 @@
 export const points = {
-  "Ya no hay buenos días, solo días 🏈💛🖤": 5.5,
-  "Muffin": 4,
-  "French fries🍟 ft el “gleason” 🦸🏻‍♂️": 6.5,
-  "Itzae": 5,
-  "Mond 🌙": 6.5,
-  "🦁": 6,
-  "DR Foxter": 8,
-  "Indiana Bloomington!!!": 4,
-  "Dejan una": 2
+  "Ya no hay buenos días, solo días 🏈💛🖤": 11,
+  "Muffin": 6,
+  "French fries🍟 ft el “gleason” 🦸🏻‍♂️": 10.5,
+  "Itzae": 6,
+  "Mond 🌙": 9.5,
+  "🦁": 8,
+  "DR Foxter": 10,
+  "Indiana Bloomington!!!": 8.5,
+  "Dejan una": 3
 }
